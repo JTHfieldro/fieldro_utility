61c3c5e805fd360fd09a15abc998a94e7e32811a
joth <joth@fieldro.tech>
2025-06-23 11:01:42 +0900

    hw_status.msg 수정

--- src/fieldro_utility/src/web_socket_manager/web_socket_manager_pub.cpp
+++ src/fieldro_utility/src/web_socket_manager/web_socket_manager_pub.cpp
@@ -5,45 +5,51 @@ void frb::WebSocketManager::publish_hw_status(const std::string& status_json)
   fieldro_utility::hw_status msg;
 
   // 기본 값 설정
-  msg.robot_is_sensor_stable_lidar_front  = "UNKNOWN";
-  msg.robot_is_sensor_stable_lidar_rear   = "UNKNOWN";
-  msg.robot_is_sensor_stable_lidar_3d     = "UNKNOWN";
-  msg.robot_is_sensor_stable_depth_front  = "UNKNOWN";
-  msg.robot_is_sensor_stable_depth_rear   = "UNKNOWN";
-  msg.robot_is_sensor_stable_motor        = "UNKNOWN";
-  msg.robot_is_sensor_stable_imu          = "UNKNOWN";
-  msg.robot_is_sensor_stable_tof          = "UNKNOWN";
-  msg.robot_hw_switch_key_status          = "UNKNOWN";
-  msg.robot_hw_switch_ems_status          = "UNKNOWN";
-  msg.robot_is_charging_status            = "UNKNOWN";
+  msg.robot_hw_switch_ems_status                   = "UNKNOWN";
+  msg.robot_hw_switch_key_status                   = "UNKNOWN";
+  msg.robot_hw_switch_reset_status                 = "UNKNOWN";
+  msg.robot_hw_switch_start_stop_status            = "UNKNOWN";
+  msg.robot_is_charging_status                     = "UNKNOWN";
+  msg.robot_is_sensor_stable_depth_front           = "UNKNOWN";
+  msg.robot_is_sensor_stable_depth_rear            = "UNKNOWN";
+  msg.robot_is_sensor_stable_imu                   = "UNKNOWN";
+  msg.robot_is_sensor_stable_lidar_3d              = "UNKNOWN";
+  msg.robot_is_sensor_stable_lidar_front           = "UNKNOWN";
+  msg.robot_is_sensor_stable_lidar_rear            = "UNKNOWN";
+  msg.robot_is_sensor_stable_motor                 = "UNKNOWN";
+  msg.robot_is_sensor_stable_tof                   = "UNKNOWN";
 
   try
   {
     // get_hw_status() 에서 전달되는 문자열은 data 객체만 포함한다
     const auto data = nlohmann::json::parse(status_json);
 
-    msg.robot_is_sensor_stable_lidar_front =
-        data.value("robot_is_sensor_stable_lidar_front", msg.robot_is_sensor_stable_lidar_front);
-    msg.robot_is_sensor_stable_lidar_rear  =
-        data.value("robot_is_sensor_stable_lidar_rear", msg.robot_is_sensor_stable_lidar_rear);
-    msg.robot_is_sensor_stable_lidar_3d    =
-        data.value("robot_is_sensor_stable_lidar_3d", msg.robot_is_sensor_stable_lidar_3d);
+    msg.robot_hw_switch_ems_status =
+        data.value("robot_hw_switch_ems_status", msg.robot_hw_switch_ems_status);
+    msg.robot_hw_switch_key_status =
+        data.value("robot_hw_switch_key_status", msg.robot_hw_switch_key_status);
+    msg.robot_hw_switch_reset_status =
+        data.value("robot_hw_switch_reset_status", msg.robot_hw_switch_reset_status);
+    msg.robot_hw_switch_start_stop_status =
+        data.value("robot_hw_switch_start_stop_status", msg.robot_hw_switch_start_stop_status);
+    msg.robot_is_charging_status =
+        data.value("robot_is_charging_status", msg.robot_is_charging_status);
     msg.robot_is_sensor_stable_depth_front =
         data.value("robot_is_sensor_stable_depth_front", msg.robot_is_sensor_stable_depth_front);
-    msg.robot_is_sensor_stable_depth_rear  =
+    msg.robot_is_sensor_stable_depth_rear =
         data.value("robot_is_sensor_stable_depth_rear", msg.robot_is_sensor_stable_depth_rear);
-    msg.robot_is_sensor_stable_motor       =
-        data.value("robot_is_sensor_stable_motor", msg.robot_is_sensor_stable_motor);
-    msg.robot_is_sensor_stable_imu         =
+    msg.robot_is_sensor_stable_imu =
         data.value("robot_is_sensor_stable_imu", msg.robot_is_sensor_stable_imu);
-    msg.robot_is_sensor_stable_tof         =
+    msg.robot_is_sensor_stable_lidar_3d =
+        data.value("robot_is_sensor_stable_lidar_3d", msg.robot_is_sensor_stable_lidar_3d);
+    msg.robot_is_sensor_stable_lidar_front =
+        data.value("robot_is_sensor_stable_lidar_front", msg.robot_is_sensor_stable_lidar_front);
+    msg.robot_is_sensor_stable_lidar_rear =
+        data.value("robot_is_sensor_stable_lidar_rear", msg.robot_is_sensor_stable_lidar_rear);
+    msg.robot_is_sensor_stable_motor =
+        data.value("robot_is_sensor_stable_motor", msg.robot_is_sensor_stable_motor);
+    msg.robot_is_sensor_stable_tof =
         data.value("robot_is_sensor_stable_tof", msg.robot_is_sensor_stable_tof);
-    msg.robot_hw_switch_key_status         =
-        data.value("robot_hw_switch_key_status", msg.robot_hw_switch_key_status);
-    msg.robot_hw_switch_ems_status         =
-        data.value("robot_hw_switch_ems_status", msg.robot_hw_switch_ems_status);
-    msg.robot_is_charging_status           =
-        data.value("robot_is_charging_status", msg.robot_is_charging_status);
   }
   catch(const std::exception&)
   {
